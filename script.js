b2e2b9a0bf3c9b643d432496cc453c1a3ce39723
mrrony321 <mizaur@zaagsys.com>
2024-09-04 22:47:04 +0600

    java script data mutation L1 in branch jspractice

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+//Variables and Data types in JavaScript
 var firstName = 'Mizanur';
 var lastName = 'Rahman';
 var age = 22;
 var fullAge = true;
-const name1 = 'horn2'
-console.log(firstName, lastName, age, name1)
+const name1 = 'horn2';
+
+var job;
+job = 'Teacher';
+//console.log(firstName, lastName, age, name1, job)
+
+/* ***************************** */
+//Variable mutation and type coercion
+
+console.log('My name is '+firstName+' '+lastName+' is a '+job+ ' and I am '+age+' years old.')
+alert('My name is '+firstName+' '+lastName+' is a '+job+ ' and I am '+age+' years old.')
+lastName = prompt("what is the last name?")
+alert('My name is '+firstName+' '+lastName+' is a '+job+ ' and I am '+age+' years old.')
